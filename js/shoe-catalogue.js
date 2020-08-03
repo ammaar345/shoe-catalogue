4a02a74ef0e1b3bdfa83f354e7026510402f252e
ammaar345 <fakirammaar@gmail.com>
2020-08-03 13:20:37 +0200

    aligned buttons and labels

---
+++ js/shoe-catalogue.js
@@ -0,0 +1,18 @@
+
+
+
+
+var shoes = [
+    {
+        color : 'blue',
+        brand : "Mike",
+        price : 350,
+        in_stock : 5
+    },
+    {
+        color : 'orange',
+        brand : "Hadidas",
+        price : 275,
+        in_stock : 3
+    }
+];
